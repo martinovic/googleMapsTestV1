773f9d098b01d142cebf87134c5c4bdd41e7f370
Marcelo Martinovic <marcelo.martinovic@gmail.com>
2015-06-17 12:50:52 -0300

    Seguimos aprendiendo android y google maps, se agrega una misma variable 2 puntos y se agrega animationcamera

--- src/com/dotworld/googlemapstestv1/MainActivity.java
+++ src/com/dotworld/googlemapstestv1/MainActivity.java
@@ -1,7 +1,20 @@
+/*
+ * Changelog
+ * 17 de junio 2015:
+ * - Agrego el animateCamera y pruebo de pasarle el valor de BA con zoom de 12 y despues de 10.
+ * - Modifico para que con una misma variable de Mark pueda ingresar 2 Lat y Long diferentes.
+ * 16 de junio 2015:
+ * - Prueba de agregar multiples marcas al mapa
+ */
 package com.dotworld.googlemapstestv1;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -48,8 +61,24 @@ public class MainActivity extends Activity {
 	private void initilizeMap(){
 		if (googleMap == null){
 			googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			// Aca se pone el tipo de mapa
+			googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+			// Aca se pueden cargar las coordenadas para las marcas
+			final LatLng CIU = new LatLng(35.21843892856462, 33.41662287712097);         
+			Marker ciu = googleMap.addMarker(new MarkerOptions().position(CIU).title("My Office"));  
+			final LatLng BA = new LatLng(-34.5901256,-58.3952647);           
+			ciu = googleMap.addMarker(new MarkerOptions().position(BA).title("Buenos Aires, yo !!!"));
+			
+			googleMap.setMyLocationEnabled(true); // false to disable
+			googleMap.getUiSettings().setCompassEnabled(true);
+			
+			CameraPosition cameraPosition = new CameraPosition.Builder().target(
+					BA).zoom(10).build();
+
+			googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+			
 			if (googleMap == null){
-				Toast.makeText(getApplicationContext(), "Jodido.. no esta el maps !!!", Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(), "Lo lamento no esta el maps !!!", Toast.LENGTH_SHORT).show();
 			}
 		}
 	}
